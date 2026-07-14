1ef4a3004276a1eea240b85fb3334c0b92980497
CsabaMolnar <csabusmolnar@gmail.com>
2026-07-14 15:08:30 +0200

    Added Basic folder structure, made controllers.

---
+++ Warehouse/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Data.Entities;
+
+namespace Warehouse.Data
+{
+    // This class is the gateway to the database.
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //Database tables
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
