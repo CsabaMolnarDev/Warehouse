0fe0e2bad4a390647245f0bf6d656b388a4f21f2
Meow <Meow@DESKTOP-OEVCJDG>
2026-07-14 12:25:46 +0200

    Add project files.

---
+++ Warehouse/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Warehouse.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }   //F.E. Electronics, Furniture, Clothing
+
+        // Relationship -> One Category can have many Products (Laravel: hasMany)
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ Warehouse/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Warehouse.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string SKU { get; set; } // Stock Keeping Unit (Unique identifier)
+        public decimal Price { get; set; }
+        public int Quantity { get; set; } // Current stock level
+
+        // Foreign Key: Links to Category
+        public int CategoryId { get; set; }
+
+        // Relationship: Product belongs to a Category (Laravel: belongsTo)
+        public Category? Category { get; set; }
+    }
+}
